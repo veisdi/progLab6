1bc066d6f151b353fda4bfe3630b26c32c28abd8
Diana Veisgeim <505169@niuitmo.ru>
2026-04-08 17:32:36 +0300

    fix errors

--- src/main/java/lab6/client/ClientApp.java
+++ src/main/java/lab6/client/ClientApp.java
@@ -1,26 +1,1 @@
-package lab6.client;
-
-import java.util.logging.*;
-
-public class ClientApp {
-    public static void main(String[] args) {
-        // Настройка логгера
-        Logger logger = Logger.getLogger(ClientApp.class.getName());
-        try {
-            FileHandler fh = new FileHandler("client.log");
-            logger.addHandler(fh);
-        } catch (Exception e) { e.printStackTrace(); }
-
-        String host = "localhost";
-        int port = 9000;
-
-        if (args.length >= 1) host = args[0];
-        if (args.length >= 2) port = Integer.parseInt(args[1]);
-
-        try {
-            new ClientManager(host, port).run();
-        } catch (Exception e) {
-            System.err.println("Не удалось подключиться к серверу: " + e.getMessage());
-        }
-    }
-}
+package lab6.client;import java.util.logging.*;public class ClientApp {    public static void main(String[] args) {        // Настройка логгера        Logger logger = Logger.getLogger(ClientApp.class.getName());        try {            FileHandler fh = new FileHandler("client.log");            logger.addHandler(fh);        } catch (Exception e) { e.printStackTrace(); }        String host = "localhost";        int port = 10024;        if (args.length >= 1) host = args[0];        if (args.length >= 2) port = Integer.parseInt(args[1]);        try {            new ClientManager(host, port).run();        } catch (Exception e) {            System.err.println("Не удалось подключиться к серверу: " + e.getMessage());        }    }}
